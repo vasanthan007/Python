9d60c807be909363ddee2a58a5bdceaaf7e72406
Vasanthan T <vasanthan.thamarai51@gmail.com>
2025-05-21 13:10:32 +0530

    Leetcode sorted square array brute force
    Day:1 Problem

---
+++ Leetcode_sortedSquareArray.py
@@ -0,0 +1,12 @@
+#Sorted Squared Array
+def sorted_squared(array):
+        n = len(array)
+        res = [0]*n
+        for i in range (n):
+                res[i] = array[i] ** 2
+        res.sort()
+        return (res)
+    
+val = input("Enter elements separated by space: ").split()
+arr = [int(num_str) for num_str in val]
+print(sorted_squared(arr))
